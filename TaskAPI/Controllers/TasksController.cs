43d2e810b915aace76e97e3321e32849a7a2b610
Shalitha Lakruwan <69634572+shalithacode@users.noreply.github.com>
2023-05-28 23:46:38 +0530

    Initial controller created.

---
+++ TaskAPI/Controllers/TasksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var tasks = new string[] { "Task 1", "Task 2", "Task 3" };
+            return Ok(tasks);
+        }
+        [HttpPost]
+        public IActionResult NewTasks()
+        { 
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateTasks()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteTasks()
+        {
+            return BadRequest();
+        }
+    }
+}

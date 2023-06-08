bb73112b203fbad6d3e418819385635b77e10d80
Shalitha Lakruwan <69634572+shalithacode@users.noreply.github.com>
2023-06-08 22:53:12 +0530

    Added SQL db connaction, done db migrations with entity framework

---
+++ TaskAPI.DataAccess/TodoDBContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskAPI.Models;
+
+namespace TaskAPI.DataAccess
+{
+    public class TodoDBContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost; Database=MyTodoDb; Trusted_Connection=true; Encrypt=False;";
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasData(new Todo()
+            {
+                Id = 1,
+                Title = "Buy Book - DB",
+                Description = "Buy boo for school.",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(2),
+                Status = TodoStatus.New
+            });
+        }
+
+    }
+}
---
+++ TaskAPI.Services/TodoSqlServerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskAPI.DataAccess;
+using TaskAPI.Models;
+
+
+namespace TaskAPI.Services
+{
+    public class TodoSqlServerService : ITodoRepository
+    {
+
+        private readonly TodoDBContext _dbContext= new TodoDBContext();
+        public List<Todo> AllTodos()
+        {
+            return _dbContext.Todos.ToList();
+        }
+    }
+}

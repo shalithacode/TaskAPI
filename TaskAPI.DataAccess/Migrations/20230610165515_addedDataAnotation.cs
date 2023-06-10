0227b1f7a608f2752a03e48875189a6002506ef6
Shalitha Lakruwan <69634572+shalithacode@users.noreply.github.com>
2023-06-10 22:30:15 +0530

    Added anotations to all models.

---
+++ TaskAPI.DataAccess/Migrations/20230610165515_addedDataAnotation.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskAPI.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedDataAnotation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Todos",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Todos",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressNo",
+                table: "Authors",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Authors",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Authors",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AddressNo", "City", "Street" },
+                values: new object[] { "45", "Colombo 1", "Street 1" });
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "AddressNo", "City", "Street" },
+                values: new object[] { "35", "Colombo 2", "Street 2" });
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "AddressNo", "City", "Street" },
+                values: new object[] { "25", "Colombo 3", "Street 3" });
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "AddressNo", "City", "Street" },
+                values: new object[] { "15", "Colombo 4", "Street 4" });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 6, 10, 22, 25, 15, 60, DateTimeKind.Local).AddTicks(8144), new DateTime(2023, 6, 15, 22, 25, 15, 60, DateTimeKind.Local).AddTicks(8163) });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 6, 10, 22, 25, 15, 60, DateTimeKind.Local).AddTicks(8172), new DateTime(2023, 6, 15, 22, 25, 15, 60, DateTimeKind.Local).AddTicks(8173) });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 6, 10, 22, 25, 15, 60, DateTimeKind.Local).AddTicks(8176), new DateTime(2023, 6, 15, 22, 25, 15, 60, DateTimeKind.Local).AddTicks(8177) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressNo",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Authors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Todos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Todos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 6, 9, 22, 14, 23, 748, DateTimeKind.Local).AddTicks(5423), new DateTime(2023, 6, 14, 22, 14, 23, 748, DateTimeKind.Local).AddTicks(5435) });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 6, 9, 22, 14, 23, 748, DateTimeKind.Local).AddTicks(5448), new DateTime(2023, 6, 14, 22, 14, 23, 748, DateTimeKind.Local).AddTicks(5449) });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 6, 9, 22, 14, 23, 748, DateTimeKind.Local).AddTicks(5452), new DateTime(2023, 6, 14, 22, 14, 23, 748, DateTimeKind.Local).AddTicks(5454) });
+        }
+    }
+}
--- TaskAPI.DataAccess/TodoDBContext.cs
+++ TaskAPI.DataAccess/TodoDBContext.cs
@@ -22,10 +22,10 @@ namespace TaskAPI.DataAccess
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Author>().HasData(new Author[] {
-                new Author { Id = 1, Name = "Fiqri Ismail"},
-                new Author { Id = 2, Name = "Prabhashwara Bandara"},
-                new Author { Id = 3, Name = "Chaminda Sooriyapperuma"},
-                new Author { Id = 4, Name = "Hansamali Gamage"}
+                new Author { Id = 1, Name = "Fiqri Ismail", AddressNo = "45", Street = "Street 1", City = "Colombo 1"},
+                new Author { Id = 2, Name = "Prabhashwara Bandara", AddressNo = "35", Street = "Street 2", City = "Colombo 2"},
+                new Author { Id = 3, Name = "Chaminda Sooriyapperuma", AddressNo = "25", Street = "Street 3", City = "Colombo 3"},
+                new Author { Id = 4, Name = "Hansamali Gamage", AddressNo = "15", Street = "Street 4", City = "Colombo 4"}
             });
 
             modelBuilder.Entity<Todo>().HasData(new Todo[]
